9d466dc3f446fbb49f127c791ea18008e8f8775a
Kumardeep Singh <Kumardeepsingh@student.kpu.ca>
2026-05-12 13:54:33 -0700

    Implemented get endpoint to retrieve photos

--- backend/restaurant/src/main/java/com/kumar/restaurant/controllers/PhotoController.java
+++ backend/restaurant/src/main/java/com/kumar/restaurant/controllers/PhotoController.java
@@ -5,10 +5,12 @@ import com.kumar.restaurant.domain.entities.Photo;
 import com.kumar.restaurant.mappers.PhotoMapper;
 import com.kumar.restaurant.services.PhotoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -24,6 +26,20 @@ public class PhotoController {
         Photo savedPhoto = photoService.uploadFile(file);
         return photoMapper.toDto(savedPhoto);
     }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id){
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(
+                                MediaTypeFactory.getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION,"inline")
+                        .body(photo)
+                ).orElse(ResponseEntity.notFound().build());
+    }
+
 
 
 }
